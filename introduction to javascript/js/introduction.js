39912b40e420500fd2cc212dc449e962ca87e4d4
isaac <watakeri@gmail.com>
2018-11-15 17:38:47 +0300

    add introduction to java

---
+++ introduction to javascript/js/introduction.js
@@ -0,0 +1,35 @@
+// /*var student;
+// student = "john Doe";
+// console.log(student);*/
+// let num1 =2500;
+// let num2 = 50;
+//
+// let sum = num1 + num2;
+// let mult = num1 * num2;
+// let dev = num1 / num2;
+//
+// console.log("my numbers are: ",num1, num2);
+// console.log("Sum is: ",sum);
+// console.log("Mult is: ",mult);
+// console.log("Dev is: ",dev);
+//area of a triangle
+let base = 5;
+let height =6;
+
+let area = 0.5 * base * height;
+console.log("area of the triangle is: ", area);
+
+//program to calculate the multiplication and division of two numbers
+let i = 40;
+let w = 20;
+let times = i * w;
+let divide = i / w;
+console.log("the multiplication of the two numbers is: ", times);
+console.log("the division of the two numbers is: ", divide);
+
+//program to calculate the age
+let name = "Stella";
+let DOB = 1990;
+let current_year = 2018;
+let age = current_year - DOB;
+console.log("Your name and age is: ",name,age);
